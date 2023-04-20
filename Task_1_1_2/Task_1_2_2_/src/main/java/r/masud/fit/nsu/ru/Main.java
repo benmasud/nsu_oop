2fea8515686307f3930ca8c1c430d88c9fd9b59b
Rafik <69720999+rafi2002@users.noreply.github.com>
2023-04-20 13:32:17 +0700

    settings.gradle

--- Task_1_1_2/Task_1_2_2_/src/main/java/r/masud/fit/nsu/ru/Main.java
+++
@@ -1,7 +0,0 @@
-package r.masud.fit.nsu.ru;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ Task_3_1_1/src/main/java/r/masud/ccfit/nsu/ru/Direction.java
@@ -0,0 +1,8 @@
+package r.masud.ccfit.nsu.ru;
+
+public class Direction {
+    public static final int UP = 1;
+    public static final int DOWN = -1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = -2;
+}
